f95cc3cae870cbb76e927168ac60f66cac10d11f
Blaire Baker <bakerblaire@gmail.com>
2021-01-25 17:18:04 -0600

    signin

--- cc-frontend/index.js
+++ cc-frontend/index.js
@@ -1,1 +1,20 @@
 //JS GOES IN HERE
+
+//add event listener for submit button
+//grab the username submitted and finds it in DB
+//if not found, create a new user
+//load user home page with menu bar
+
+//add event listener for view clothing
+//if clicked, fetch and render all clothing items
+
+document.addEventListener('DOMContentLoaded', () => {
+    userLogin()
+})
+
+function userLogin() {
+    document.getElementById('login').addEventListener('submit', (event) => {
+        event.preventDefault()
+        debugger
+    })
+}
