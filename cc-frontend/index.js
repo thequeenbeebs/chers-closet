f79f371eca62393f221aee80899a86ae8be4be80
ebyanyusuf <ebyan.yusuf@gmail.com>
2021-01-26 10:56:46 -0500

    second

--- cc-frontend/index.js
+++ cc-frontend/index.js
@@ -11,9 +11,37 @@ function fetchClothing() {
 
 //need to finish creating card/rendering clothing
 function renderClothing(clothing) {
-    document.querySelector("#card-container")
+    let getLocation = document.querySelector("#card-container")
+    let div = document.createElement("div")
+    div.classList.add("card")
+    let header = document.createElement("h2")
+    header.innerHTML = clothing.name
+    let image = document.createElement("img")
+    image.src = clothing.image
+    let brand = document.createElement("h4")
+    brand.innerHTML = clothing.brand
+    let category = document.createElement("p")
+    category.innerHTML = clothing.category
+    let paragraph = document.createElement("p")
+    paragraph.innerHTML = clothing.color
+    let button1 = document.createElement("button")
+    button1.innerHTML = "Edit Item"
+    let button2 = document.createElement("button")
+    button2.innerHTML = "Delete Item"
+    
+    div.append(header, image, brand, category, paragraph, button1, button2)
+    getLocation.append(div)
+    
 
 }
+{/* <div class="card">
+                <h2>Dummy Card</h2>
+                <img src="img_avatar.png" alt="Avatar" style="width:100%">
+                <h4><b>John Doe</b></h4> 
+                <p>Architect & Engineer</p> 
+                <button>Edit Item</button>
+                <button>Delete Item</button>
+            </div> */}
 
 //log in functionality -- work in progress
 function userLogin() {
