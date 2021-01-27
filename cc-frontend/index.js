24f6ab74c2529f912c0c27a035237675800951ec
Blaire Baker <bakerblaire@gmail.com>
2021-01-27 08:48:35 -0600

    started adding out CSS

--- cc-frontend/index.js
+++ cc-frontend/index.js
@@ -43,6 +43,7 @@ function userLogin() {
         .then(resp => resp.json())
         .then(user => {
             CURRENT_USER = user
+            document.querySelector('h1').innerHTML = `${user.username}'s Closet`
             renderUserPage(user)
         })
     })
@@ -50,16 +51,16 @@ function userLogin() {
 
 function renderUserPage(user) {
     let navbar = document.getElementById("navbar")
-    let welcome = document.createElement('h2')
-        welcome.innerHTML = `Welcome, ${user.username}!`
     let clothingButton = document.createElement("button")
         clothingButton.innerHTML = "View Clothing"
         clothingButton.classList.add('right')
+        clothingButton.classList.add('nav-buttons')
         clothingButton.addEventListener('click', () => viewClothing(user.clothings))
     let outfitButton = document.createElement("button")
         outfitButton.innerHTML = "View Outfits"
         outfitButton.classList.add('right')
-    navbar.append(welcome, clothingButton, outfitButton)
+        outfitButton.classList.add('nav-buttons')
+    navbar.append(outfitButton, clothingButton)
     document.getElementById("login").remove()
 }
 
